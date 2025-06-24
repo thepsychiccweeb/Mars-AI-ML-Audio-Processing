7c73335eeffba0b9ad567fd1c411b4f20b8cadd0
thepsychiccweeb <153130068+thepsychiccweeb@users.noreply.github.com>
2025-06-24 23:24:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,54 @@ import tensorflow as tf
 # Load your trained model
 from my_custom_layers import AttentionPooling  # or define it inline
 
-model = tf.keras.models.load_model('final_emotion_model_1.keras', custom_objects={'AttentionPooling': AttentionPooling})
+model = tf.keras.models.load_model(
+    'final_emotion_model_1.keras',
+    custom_objects={'AttentionPooling': AttentionPooling},
+    compile=False  # safer for inference
+)
+
+from tensorflow.keras import layers
+
+class AttentionPooling(layers.Layer):
+    """Attention-based pooling layer for better feature aggregation"""
+    def __init__(self, units=64, **kwargs):
+        super(AttentionPooling, self).__init__(**kwargs)
+        self.units = units
+        
+    def build(self, input_shape):
+        self.W = self.add_weight(
+            name='attention_weight',
+            shape=(input_shape[-1], self.units),
+            initializer='glorot_uniform',
+            trainable=True
+        )
+        self.b = self.add_weight(
+            name='attention_bias',
+            shape=(self.units,),
+            initializer='zeros',
+            trainable=True
+        )
+        self.u = self.add_weight(
+            name='attention_context',
+            shape=(self.units,),
+            initializer='glorot_uniform',
+            trainable=True
+        )
+        super(AttentionPooling, self).build(input_shape)
+    
+    def call(self, inputs):
+        x = tf.expand_dims(inputs, axis=1)  # (batch_size, 1, features)
+        uit = tf.tanh(tf.tensordot(x, self.W, axes=1) + self.b)  # (batch_size, 1, units)
+        ait = tf.tensordot(uit, self.u, axes=1)  # (batch_size, 1)
+        ait = tf.nn.softmax(ait, axis=1)  # (batch_size, 1)
+        weighted_input = x * tf.expand_dims(ait, axis=-1)  # (batch_size, 1, features)
+        return tf.reduce_sum(weighted_input, axis=1)  # (batch_size, features)
+    
+    def get_config(self):
+        config = super(AttentionPooling, self).get_config()
+        config.update({'units': self.units})
+        return config
+
 
 
 
